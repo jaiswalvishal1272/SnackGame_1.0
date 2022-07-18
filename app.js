83eac686c9477f14fd9d10c55a9f2d914fe4c02f
Sayali-junghare <104709316+Sayali-junghare@users.noreply.github.com>
2022-07-18 12:07:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 console.log("This is attached");
+const backgroundMusic=new Audio("/game_bgMusic.mp3");
+
+const eatingMusic=new Audio("/eat_music.mp3");
+const gameoverMusic=new Audio("/gameover.wav");
+let speed=2;
+//backgroundMusic.play();
+//to run my game in loop
+function loop(ctime){
+    window.requestAnimationFrame(loop);
+    console.log(ctime);
+    if((ctime-lastPaintTime)/1000 <1/speed){
+        return;
+    }
+    lastPaintTime=ctime;
+}
+//loop();
