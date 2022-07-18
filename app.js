7f63afafff9c7c4b444cc507fd24292d35ca45bb
jaiswalvishal1272 <jaiswalvishal1272@gmail.com>
2022-07-18 12:30:39 +0530

    Snack Direction code

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log("This is attached");
+// Game Constraints & Variables
+food = {x: 3, y: 5};
+let inputDirection = {x: 0, y: 0};
+
+// change the direction of snake
+window.requestAnimationFrame(main);
+window.addEventListener("keydown", e => {
+    inputDir = {x: 0, y: 1}
+    switch(e.key) {
+        case "ArrowUp":
+            console.log("ArrowUp");
+            inputDirection.x = 0;
+            inputDirection.y = -1;
+            break;
+
+        case "ArrowDown":
+            console.log("ArrowDown");
+            inputDirection.x = 0;
+            inputDirection.y = 1;
+            break;
+        
+        case "ArrowLeft":
+            console.log("ArrowLeft");
+            inputDirection.x = -1;
+            inputDirection.y = 0;
+            break;
+            
+        case "ArrowRight":
+            console.log("ArrowRight");
+            inputDirection.x = 1;
+            inputDirection.y = 0;
+            break;
+        default :
+        break;
+    }
+})
